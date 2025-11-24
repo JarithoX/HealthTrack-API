b1cc11d6519e67c905ff770f57852449818eaa3d
Benjinho-19 <benjamin.gonzalez119@inacapmail.cl>
2025-11-24 02:30:21 -0300

    Ajustes en habitos controllers para la parte de habitos en django, aun no terminado. lo terminare

--- routes/habitos.routes.js
+++ routes/habitos.routes.js
@@ -6,5 +6,5 @@ router.post('/habito-definicion', ctrl.crearDefinicion);
 router.get('/habito-definicion/:username', ctrl.listarDefiniciones);
 router.post('/habito-registro', ctrl.registrarHabito);
 router.get('/habito-registro/:username', ctrl.listarRegistrosConDefinicion);
-
+router.delete('/habito-definicion/:id', ctrl.eliminarDefinicion);
 module.exports = router;
